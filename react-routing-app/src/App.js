1e96488efd72fc6bcb028c190662f62a29bdeaea
Rohan Singh <rohaniiitv@gmail.com>
2018-04-18 11:28:21 +0530

    added particlesjs

--- react-routing-app/src/App.js
+++ react-routing-app/src/App.js
@@ -8,14 +8,18 @@ import {
     Link,
     Route,
     } from 'react-router-dom';
+import Particles from 'react-particles-js';
 
 class App extends Component {
 
     componentWillMount () {
-        document.body.style.backgroundImage = "url('http://xinature.com/wp-content/uploads/2016/10/winter-snow-weather-storm-snowstorm-3d-wallpaper-download.jpg')";
+        document.body.style.backgroundColor = '#19324c'
+        //document.body.style.backgroundImage = "url('http://xinature.com/wp-content/uploads/2016/10/winter-snow-weather-storm-snowstorm-3d-wallpaper-download.jpg')";
     }
   render() {
     return (
+        <div>
+
         <Router>
             <div>
         <ul>
@@ -30,6 +34,117 @@ class App extends Component {
 
             </div>
         </Router>
+           <Particles params={{
+                "particles": {
+                    "number": {
+                        "value": 160,
+                        "density": {
+                            "enable": true,
+                            "value_area": 800
+                        }
+                    },
+                    "color": {
+                        "value": "#ffffff"
+                    },
+                    "shape": {
+                        "type": "circle",
+                        "stroke": {
+                            "width": 0,
+                            "color": "#000000"
+                        },
+                        "polygon": {
+                            "nb_sides": 5
+                        },
+                        "image": {
+                            "src": "img/github.svg",
+                            "width": 100,
+                            "height": 100
+                        }
+                    },
+                    "opacity": {
+                        "value": 0.4,
+                        "random": true,
+                        "anim": {
+                            "enable": true,
+                            "speed": 1,
+                            "opacity_min": 0,
+                            "sync": false
+                        }
+                    },
+                    "size": {
+                        "value": 3,
+                        "random": true,
+                        "anim": {
+                            "enable": false,
+                            "speed": 4,
+                            "size_min": 0.3,
+                            "sync": false
+                        }
+                    },
+                    "line_linked": {
+                        "enable": false,
+                        "distance": 150,
+                        "color": "#ffffff",
+                        "opacity": 0.8,
+                        "width": 1
+                    },
+                    "move": {
+                        "enable": true,
+                        "speed": 1,
+                        "direction": "bottom",
+                        "random": true,
+                        "straight": false,
+                        "out_mode": "out",
+                        "bounce": false,
+                        "attract": {
+                            "enable": false,
+                            "rotateX": 600,
+                            "rotateY": 600
+                        }
+                    }
+                },
+                "interactivity": {
+                    "detect_on": "canvas",
+                    "events": {
+                        "onhover": {
+                            "enable": true,
+                            "mode": "bubble"
+                        },
+                        "onclick": {
+                            "enable": true,
+                            "mode": "repulse"
+                        },
+                        "resize": true
+                    },
+                    "modes": {
+                        "grab": {
+                            "distance": 400,
+                            "line_linked": {
+                                "opacity": 1
+                            }
+                        },
+                        "bubble": {
+                            "distance": 250,
+                            "size": 0,
+                            "duration": 2,
+                            "opacity": 0,
+                            "speed": 3
+                        },
+                        "repulse": {
+                            "distance": 400,
+                            "duration": 0.4
+                        },
+                        "push": {
+                            "particles_nb": 4
+                        },
+                        "remove": {
+                            "particles_nb": 2
+                        }
+                    }
+                },
+                "retina_detect": true
+            }}/>
+        </div>
     );
   }
 }
