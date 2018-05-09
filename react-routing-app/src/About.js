cb9d0e4dd8e55df686f44236e88f4f0e2b746e27
Rohan Singh <rohaniiitv@gmail.com>
2018-05-09 19:14:29 +0530

    altered text in about page

--- react-routing-app/src/About.js
+++ react-routing-app/src/About.js
@@ -5,8 +5,15 @@ class About extends React.Component {
     render () {
         return(
             <div>
-                <h4>About Page</h4>
-
+                <h4>About NowCaster</h4>
+                <p>
+                NowCaster is a simple web-application which 
+                helps to determine details about a city for 
+                instance, for London, it will render the 
+                details about the city London.
+                This web-application is made with love in 
+                react and material-ui!
+                </p>
             </div>
 
         );
